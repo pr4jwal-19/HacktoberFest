4f57af930040f1c42f7fdc5c5d17fb8f16c21a85
Prajwal Nakure <144579726+pr4jwal-19@users.noreply.github.com>
2023-10-18 12:46:38 +0530

    Update pallindrome.java
    This code is for checking if a string is palindrome or not using a recursive function.

--- Java_Programs_for_beginners/pallindrome.java
+++ Java_Programs_for_beginners/pallindrome.java
@@ -1,40 +1,29 @@
- 
-public class Pallindrome { 
-  
-    // Function that returns true if 
-    // str is a palindrome 
-    static boolean isPalindrome(String str) 
-    { 
-  
-        // Pointers pointing to the beginning 
-        // and the end of the string 
-        int i = 0, j = str.length() - 1; 
-  
-        // While there are characters toc compare 
-        while (i < j) { 
-  
-            // If there is a mismatch 
-            if (str.charAt(i) != str.charAt(j)) 
-                return false; 
-  
-            // Increment first pointer and 
-            // decrement the other 
-            i++; 
-            j--; 
-        } 
-  
-        // Given string is a palindrome 
-        return true; 
-    } 
-  
-    // Driver code 
-    public static void main(String[] args) 
-    { 
-        String str = "TATA"; 
-  
-        if (isPalindrome(str)) 
-            System.out.print("Yes"); 
-        else
-            System.out.print("No"); 
-    } 
-} 
+public class Palindrome {
+    // Function to check if a string is a palindrome using recursion
+    static boolean isPalindrome(String str) {
+        // Base case: If the string is empty or has only one character, it's a palindrome
+        if (str.length() <= 1) {
+            return true;
+        }
+        
+        // Compare the first and last characters of the string
+        if (str.charAt(0) == str.charAt(str.length() - 1)) {
+            // If they match, check the substring between the first and last characters
+            // by recursively calling isPalindrome on the substring
+            return isPalindrome(str.substring(1, str.length() - 1));
+        }
+        
+        // If the first and last characters don't match, the string is not a palindrome
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String str = "racecar"; // Change this string as needed
+
+        if (isPalindrome(str)) {
+            System.out.print("Yes, it's a palindrome.");
+        } else {
+            System.out.print("No, it's not a palindrome.");
+        }
+    }
+}
